5d04a02afd55107ee31402d6556131b1326b37b2
sami2272 <usmanso2006@gmail.com>
2024-02-22 15:39:44 +0500

    dialogs for low,ful charger connect and disconnect are converted to activity and integrated with the service

---
+++ app/src/main/java/com/example/batteryanimation/Activities/ChargerDisconnectDialogActivity.kt
@@ -0,0 +1,12 @@
+package com.example.batteryanimation.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.batteryanimation.R
+
+class ChargerDisconnectDialogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charger_disconnect_dialog)
+    }
+}
